e57608a1499d9399a992e8a540d6b620305e9b0f
Gavin Rehkemper <grehkemper@esri.com>
2025-01-09 12:44:42 -0600

    delete these configs to convert them into templates in the next commit

--- samples/jsapi-integration/config.js
+++
@@ -1,9 +0,0 @@
-/*
-You can generate your own clientid by creating an application on the ArcGIS for Developers site. Be sure to add http://localhost:3000 as a redirect uri for your application.
-
-Once you have a clientid of your own, copy/paste it here.
-*/
-const config = {
-  clientId: "3CiiHWyTNMIRNyF1",
-  redirectUri: "http://localhost:3000/authenticate"
-};
