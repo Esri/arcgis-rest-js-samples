db1dd9647934ce84c310125f17bfaedd6708d4e2
Patrick Arlt <parlt@esri.com>
2025-02-21 07:11:03 -0800

    Developer Credentials v2 demo (#8)
    * developer credentials v2 demo
    
    * update dev credentials demo
    
    * update dev credentials demo
    
    * Update samples/developer-credentials-v2/readme.md
    
    Co-authored-by: Gavin Rehkemper <137905994+gavinr-maps@users.noreply.github.com>
    
    * Update samples/developer-credentials-v2/index.ts
    
    Co-authored-by: Gavin Rehkemper <137905994+gavinr-maps@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Gavin Rehkemper <137905994+gavinr-maps@users.noreply.github.com>

---
+++ samples/developer-credentials-v2/index.ts
@@ -0,0 +1,50 @@
+import {
+  createApiKey,
+  updateApiKey,
+  getApiKey,
+} from "@esri/arcgis-rest-developer-credentials";
+import { ArcGISIdentityManager } from "@esri/arcgis-rest-request";
+import { removeItem } from "@esri/arcgis-rest-portal";
+import "dotenv/config";
+
+const authentication = await ArcGISIdentityManager.signIn({
+  username: (process.env as any).ARCGIS_USERNAME,
+  password: (process.env as any).ARCGIS_PASSWORD,
+  portal: "https://arcgis.com/sharing/rest",
+});
+
+const created = await createApiKey({
+  authentication,
+  title: "xyz_title",
+  description: "xyz_desc",
+  tags: ["xyz_tag1", "xyz_tag2"],
+  privileges: ["premium:user:geocode:temporary"],
+  generateToken1: true,
+  apiToken1ExpirationDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // 30 days
+});
+
+console.log(created);
+
+const updated = await updateApiKey({
+  authentication,
+  itemId: created.item.id,
+  httpReferrers: ["https://example.com"],
+  generateToken2: true,
+  apiToken2ExpirationDate: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30), // 30 days
+});
+
+console.log(updated);
+
+const fetched = await getApiKey({
+  authentication,
+  itemId: created.item.id,
+});
+
+console.log(fetched);
+
+const deleted = await removeItem({
+  id: created.item.id,
+  authentication,
+});
+
+console.log(deleted);
---
+++ samples/developer-credentials-v2/readme.md
@@ -0,0 +1,17 @@
+# Developer Credential Package Sample
+
+This sample shows how to use the `arcgis-rest-developer-credentials` package.
+
+## Running this sample
+
+1. Copy `.env.template` to `.env` and add your own username and password.
+2. Run `npm install` to install the dependencies.
+3. Run `npm start`.
+
+### Local ArcGIS REST JS
+
+If you'd like to use a local version of ArcGIS REST JS, follow the [steps in the parent README](../README.md#local-arcgis-rest-js-npm), referencing these packages that this sample uses as necessary:
+
+- `arcgis-rest-request`
+- `arcgis-rest-developer-credentials`
+
